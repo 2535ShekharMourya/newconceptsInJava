93e28fc47321214e8e63d0caa032f05a4c6036b4
Azadshekhar <shekharmaurya8978@gmail.com>
2023-05-05 17:25:27 +0530

    sai hai

---
+++ Freedom.java
@@ -0,0 +1,15 @@
+package Azad;
+import java.util.*;
+//import java.util.Stack;
+public class Freedom {
+    public static void main(String[] args) {
+    //nt [][]arr={{1,1,1,1},{1,0,0,1},{1,1,0,1},{1,1,1,1}};
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+       System.out.println(matrix);
+
+}}
+
+
---
+++ Practic.java
@@ -0,0 +1,10 @@
+package Azad;
+import java.util.*;
+import java.lang.Number.*;
+                                       // Merge Two Arrays in Java
+public class Practic {
+    public static void main(String[] args) {
+       System.out
+            }
+        }
+
